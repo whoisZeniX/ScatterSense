476304742b8b56edc4414ace109870372802567a
baqar mustafa <baqarmustafa@baqars-MacBook-Air.local>
2026-04-10 20:44:32 +0530

    Finalize documentation and production stability checks check bugs and solve them to make the project to ready for deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 import io
+import os
 from flask import Flask, render_template, request, redirect, jsonify, Response
 
 from database import initialize_database, insert_session, fetch_sessions, fetch_filtered_sessions, delete_session
@@ -70,4 +71,5 @@ def export_data():
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
